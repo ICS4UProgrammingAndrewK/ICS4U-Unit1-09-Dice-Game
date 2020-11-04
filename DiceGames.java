0d69e3765f418da9df3943ead32f2343e220bd83
Ubuntu <ubuntu@ip-172-31-23-173.ec2.internal>
2020-11-04 21:22:03 +0000

    completed

--- DiceGames.java
+++ DiceGames.java
@@ -1,30 +1,34 @@
 import java.util.Random;
 import java.util.Scanner;
- /**
-*
-* class DiceGames
-*
+/**
+
+* Dice Games.
+
 * @author  Andrew Kuekam
 * @version 1.0
-* @since   2020-18-09
+* @since   2020-11-02 
 */
-public class DiceGames {
-  public static void main(String[] args) {
 
-    Scanner userInput = new Scanner(System.in);
+//  
+public class DiceGames {
+  /**
+  * .
+  */
+  public static void main(String[] args) {
     
-    Scanner userNum = new Scanner(System.in);
-    
-    //This ask the user to choose its gamemode
+        //This ask the user to choose its gamemode
     System.out.println("Choose your game mode:");
     System.out.println("1.Easy mode (3 guess).");
     System.out.println("2.Normal mode (2 guesses).");
     System.out.println("3.Hard mode (1 guesses).");
     
+    Scanner userNum = new Scanner(System.in);
+    
     //This genarate number between 1 and 6
     Random rand = new Random();
     
     //Declare the String
+    Scanner userInput = new Scanner(System.in);
     String userChoice;
     userChoice = userInput.next();
     
@@ -32,34 +36,37 @@ public class DiceGames {
     int guess = 0;
     //String usergamemode = userChoice;
     
-    if (userChoice.equals("1")){
+    if (userChoice.equals("1")) {
       System.out.print(" Enter a random number between 1 and 6:");
-      for (int num = 1; num <= 3 ; num=num+1){
-        if (userInput.nextInt() == num){
+      for (int num = 1; num <= 3; num = num + 1) {
+        if (userInput.nextInt() == num) {
           System.out.println("That's right! You win!");
         } else {
-            System.out.println( " You got it Wrong! Try a gain" );
-          }
+          System.out.println("You got it Wrong! Try a gain");
+          
+        }
+        
       }
-    }
-    else if (userChoice.equals("2")){
+      
+    } else if (userChoice.equals("2")) {
       System.out.println("Enter a number between 1 and 6:");
-      for (int num = 1; num <= 2 ; num=num+1){
-        if (userInput.nextInt() == num){
+      for (int num = 1; num <= 2; num = num + 1) {
+        if (userInput.nextInt() == num) {
           System.out.println("WOW! That's right! You Win!");
         } else {
-            System.out.println( " You got it Wrong! Try a gain" );
-          }
+          System.out.println(" You got it Wrong! Try a gain");
+        }
+        
       }
-    }
-    else if (userChoice.equals("3")){
+    } else if (userChoice.equals("3")) {
       System.out.println("Enter a number between 1 and 6:");
-      for (int num = 1; num <= 1 ; num=num+1){
-        if (userInput.nextInt() == num){
+      for (int num = 1; num <= 1; num = num + 1) {
+        if (userInput.nextInt() == num) {
           System.out.println("WOW! That's right! You Win!");
         } else {
-            System.out.println( " You got it Wrong! You lose" );
-          }
+          System.out.println(" You got it Wrong! You lose");
+          
+        }
       }
     }
   }
